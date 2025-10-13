88365a2e8111fb5b6a7afbaa2bea1c98772d431c
Alvi  Manuel González <alvinmanuelgonzalez88@gmail.com>
2025-10-13 16:50:16 -0400

    Primer commit: archivo de prueba

---
+++ prueba.py
@@ -0,0 +1,23 @@
+grade = float(input("Enter the student's grade: "))
+
+
+if grade < 0 or grade > 100:
+    print("Invalid value")
+else:
+   
+    status = "Student promoted" if grade >= 70 else "Student failed"
+
+
+    if grade == 100:
+        letter = "A+"
+    elif grade >= 90:
+        letter = "A"
+    elif grade >= 80:
+        letter = "B"
+    elif grade >= 70:
+        letter = "C"
+    else:
+        letter = "F"
+
+
+    print(f"{status} - Grade: {letter}")
